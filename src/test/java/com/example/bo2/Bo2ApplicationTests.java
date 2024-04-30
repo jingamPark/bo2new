0751a291a8b1ed58e21fd43d3dd89f16a340772d
aaa <aaa@aaa.aa>
2024-04-30 11:37:54 +0900

    Initial commit

---
+++ src/test/java/com/example/bo2/Bo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
