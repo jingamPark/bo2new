7caaf719820dda2eb29723fb5c9fea648f8d9cc1
aaa <aaa@aaa.aa>
2024-05-02 09:56:12 +0900

    게시판 화면수정페이지 페이지 검색정보 전달 전까지

--- src/main/java/com/example/bo2/controller/BoardController.java
+++ src/main/java/com/example/bo2/controller/BoardController.java
@@ -6,11 +6,13 @@ import com.example.bo2.dto.PageRequestDTO;
 import com.example.bo2.dto.PageResponseDTO;
 import com.example.bo2.entity.Board;
 import com.example.bo2.service.BoardService;
+import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,11 +34,23 @@ public class BoardController {
 
     }
     @PostMapping("/register")
-    public String registerPost(BoardDTO boardDTO){
+    public String registerPost(@Valid BoardDTO boardDTO,
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes){
 
-        boardService.register(boardDTO);
+        if(bindingResult.hasErrors()){
+            log.info("has errors........");
+            redirectAttributes
+                    .addFlashAttribute("errors", bindingResult.getAllErrors());
+            log.info(bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
 
-        return "redirect:list";
+        Long bno =  boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
     }
     @GetMapping("/list")
     public void list(Model model, PageRequestDTO pageRequestDTO  ){
@@ -49,14 +63,37 @@ public class BoardController {
 
     }
     @GetMapping({"/read", "/modify"})
-    public void read(Long bno){
+    public void read(Long bno, Model model, PageRequestDTO pageRequestDTO){
+
+        model.addAttribute("dto", boardService.read(bno));
+
 
     }
-    @PostMapping("modify")
-    public String modify(Board board , RedirectAttributes redirectAttributes){
+    @PostMapping("/modify")
+    public String modify(@Valid BoardDTO boardDTO ,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes,
+                         PageRequestDTO pageRequestDTO){
         //boardService.modify(board);
+        log.info("진입 포스트");
+        // page 번호 넘기기
+        if(bindingResult.hasErrors()){
+            log.info("has errors........");
+            redirectAttributes
+                    .addFlashAttribute("errors", bindingResult.getAllErrors());
+            log.info(bindingResult.getAllErrors());
+            redirectAttributes
+                    .addAttribute("bno", boardDTO.getBno());
 
-        return null;
+            return "redirect:/board/modify?"+pageRequestDTO.getLink();
+        }
+
+        boardService.modify(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", "modfied");
+        redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+        return "redirect:/board/read?"+pageRequestDTO.getLink();
     }
     @PostMapping("/remove")
     public void remove(Long bno){
--- src/main/java/com/example/bo2/dto/BoardDTO.java
+++ src/main/java/com/example/bo2/dto/BoardDTO.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -19,8 +22,12 @@ import java.time.LocalDate;
 public class BoardDTO {
     private Long bno;
 
+    @NotEmpty
+    @Size(min=3, max = 100)
     private String title;
+    @NotEmpty
     private String content;
+    @NotEmpty
     private String writer;
 
     private LocalDate regDate;
